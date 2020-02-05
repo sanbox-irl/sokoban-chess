498ae0da5e0130452dbfec3f1efc96a01aa2b0bb
Jonathan Spira <jjspira@gmail.com>
2020-02-05 08:55:24 -0800

    Removed borked stuff

--- src/components/grid_object.rs
+++ src/components/grid_object.rs
@@ -26,16 +26,6 @@ impl ComponentBounds for GridObject {
             self.grid_type = new_grid_type;
         }
 
-        if let GridType::Button(pos) = &mut self.grid_type {
-            let mut pos_v2: Vec2Int = Vec2Int::new(pos.0 as i32, pos.1 as i32);
-            if pos_v2.vec2int_inspector(
-                ip.ui,
-                &imgui::im_str!("Block Drop Location Offset##{}", ip.uid),
-            ) {
-                *pos = (pos_v2.x as isize, pos_v2.y as isize);
-            }
-        }
-
         if self
             .move_to_point_pos
             .vec2int_inspector(ip.ui, &imgui::im_str!("##Move to Point{}", ip.uid))
@@ -84,7 +74,6 @@ pub enum GridType {
     Blockable,
     NonInteractable,
     Flag,
-    Button((isize, isize)),
 }
 
 impl Default for GridType {
